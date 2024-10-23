5604bccada925ca0172dbad84782baa9c59df570
jamie wang <bombyjamie@gmail.com>
2024-10-23 13:10:17 -0400

    added hanger

--- GameScreen.java
+++ GameScreen.java
@@ -1,8 +1,9 @@
 import greenfoot.*;
 
 public class GameScreen extends World {
+    private GreenfootImage hanger;
     public GameScreen() {
         super(600, 400, 1);
     }
-    
+    hanger = new GreenfootImage("GameScreen.png");
 }
